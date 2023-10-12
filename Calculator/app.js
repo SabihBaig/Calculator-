3cf57265909177696fc37d57ac56376c1ab4ebbc
SabihBaig <143556056+SabihBaig@users.noreply.github.com>
2023-10-12 16:01:01 +0500

    Add files via upload

---
+++ Calculator/app.js
@@ -0,0 +1,34 @@
+// let inputBox = getElmentById ("inputBox")
+// let buttons = document. querySelectorAll ("button") 
+
+// let string = ''
+
+// buttons.forEach(element =>{
+//     element.addEventListner('click',(b)=>{
+//         if(b.target.innerText == '='){
+//             string = String(eval(string))
+//             inputBox.value = string;
+//         }
+        
+//         else if(b.target.innerText =='AC'){
+//             string = ' '
+//             inputBox.value = string;
+//         }
+
+//         else if(b.target.innerText =='DEL'){
+//             string = string.substring(0,string.length)
+//             inputBox.value = string;
+//         }
+        
+//         else if(b.target.id =='plusMinus'){
+//             string = String(-eval(string))
+//             inputBox.value = string;
+//         }
+//         else{
+//             string += b.target.innerText
+//             inputBox.value = string
+//         }
+        
+//     })
+
+// })
